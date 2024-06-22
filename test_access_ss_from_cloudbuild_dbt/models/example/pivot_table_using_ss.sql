43c6e7f2a642f7a7ac80291df3524d533ea3bfe0
taketsuru-devel <followedwind@gmail.com>
2024-06-23 01:26:37 +0900

    modify pivot axis

--- test_access_ss_from_cloudbuild_dbt/models/example/pivot_table_using_ss.sql
+++ test_access_ss_from_cloudbuild_dbt/models/example/pivot_table_using_ss.sql
@@ -1,3 +1,10 @@
-select *
-from {{ source('external_tables', 'pivot_target') }}
+{% set axes = get_single_column_values(table=source('external_tables', 'pivot_target'), column='axis_data') %}
 
+select 
+  key_date,
+  {{ dbt_utils.pivot(
+    'axis_data',
+    axes
+  ) }}
+from {{ source('external_tables', 'pivot_target') }}
+group by key_date
